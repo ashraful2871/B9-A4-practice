8c473915c8df8904758b1cdcc5d8aafbe30b3447
ashraful2871 <ashrafulislam2758@gmail.com>
2024-09-09 11:30:07 +0600

    complete

--- index.js
+++ index.js
@@ -58,19 +58,73 @@ function deleteInvalids(array) {
   return numberArray;
 }
 
+// console.log(
+//   deleteInvalids([
+//     1,
+//     null,
+//     undefined,
+//     18,
+//     -19,
+//     NaN,
+//     "12",
+//     [1, 2],
+//     { ob: "lala" },
+//   ])
+// );
+// console.log(deleteInvalids(["1", { num: 2 }, NaN]));
+// console.log(deleteInvalids([1, 2, -3]));
+// console.log(deleteInvalids({ num: [1, 2, 3] }));
+
+// ----------------problem 4---------------------
+
+//output: Google#kolimuddin@1999
+
+function password(obj) {
+  if (
+    obj.name === undefined ||
+    obj.siteName === undefined ||
+    obj.birthYear === undefined ||
+    obj.birthYear.toString().length !== 4
+  ) {
+    return "Invalid";
+  }
+  const siteNameCapital = obj.siteName[0].toUpperCase() + obj.siteName.slice(1); // bujai nita hobe ajk support season e
+  const result = siteNameCapital + "#" + obj.name + "@" + obj.birthYear;
+  return result;
+}
+
 console.log(
-  deleteInvalids([
-    1,
-    null,
-    undefined,
-    18,
-    -19,
-    NaN,
-    "12",
-    [1, 2],
-    { ob: "lala" },
-  ])
+  password({ name: "kolimuddin", birthYear: 1999, siteName: "google" })
 );
-console.log(deleteInvalids(["1", { num: 2 }, NaN]));
-console.log(deleteInvalids([1, 2, -3]));
-console.log(deleteInvalids({ num: [1, 2, 3] }));
+
+// console.log(password({ name: "rahat", birthYear: 2002, siteName: "Facebook" }));
+// console.log(password({ name: "toky", birthYear: 200, siteName: "Facebook" }));
+// console.log(password({ name: "maisha", birthYear: 2002 }));
+
+// -------------------problem 5-----------------------
+
+function monthlySavings(arr, livingCost) {
+  if (!Array.isArray(arr) || typeof livingCost !== "number") {
+    return "invalid input";
+  }
+  let totalIncome = 0;
+  for (let payment of arr) {
+    if (payment >= 3000) {
+      let tax = payment * 0.2;
+      totalIncome = totalIncome + (payment - tax);
+    } else {
+      totalIncome += payment;
+    }
+  }
+  let savings = totalIncome - livingCost;
+  if (savings >= 0) {
+    return savings;
+  } else {
+    return "earn more";
+  }
+}
+
+// console.log(monthlySavings([1000, 2000, 3000], 5400));
+// console.log(monthlySavings([1000, 2000, 2500], 5000));
+// console.log(monthlySavings([900, 2700, 3400], 10000));
+// console.log(monthlySavings(100, [900, 2700, 3400]));
